c04dfa0a567ec55fb69bc00b503d7925824e3c63
Asa <71518703+LightningBerk@users.noreply.github.com>
2022-12-14 10:31:50 -0500

    Update rps.py
    Commented and optimized code, added loop function so user can play as many times as they want and use Ctrl+C to exit

--- rps.py
+++ rps.py
@@ -1,36 +1,49 @@
+# importing modules
 import requests
 import json
 import random
-rps = requests.get('https://rps101.pythonanywhere.com/api/v1/objects/all').text
-rps = rps.replace("[", "")
-rps = rps.replace("]", "")
-rps = rps.replace("'", "")
-rps = rps.split(",")
+import os
+
+# formatting api return data
+rps = requests.get('https://rps101.pythonanywhere.com/api/v1/objects/all').text # grabs list of all the rock paper scissors objects
+
+char_remove = ["[", "]", "'"] # list of characters to remove from the returned text
+for char in char_remove:  # scrubs through the string and removes the listed characters
+    rps = rps.replace(char, "")
+
+rps = rps.split(",")  # splits up the string into individual objects
+
 lst = []
-for word in rps:
+for word in rps:  # makes a list with all of your sperated objects
     lst.append(word)
 
+# defining player 1 and 2 variables
 player1 = ""
 player2 = ""
 
-
+# defining functions
 def chooseRandom():
+    # chooses a random object from the list for each player
     player1 = lst[random.randint(0, len(lst))].strip('\"')
     player2 = lst[random.randint(0, len(lst))].strip('\"')
-    print("Player One's choice is " + player1)
-    print("Player Two's choice is " + player2)
+    print("Player One's choice is " + player1 + "\nPlayer Two's choice is " + player2)
     return player1, player2
 
-
 def fightResult(x, y):
-    x, y = chooseRandom()
+    x, y = chooseRandom()  # passing through our random choices from chooseRandom()
     fightResult = requests.get(
-        'https://rps101.pythonanywhere.com/api/v1/match?object_one=' + x + '&object_two=' + y)
-    result = fightResult.json()
+        'https://rps101.pythonanywhere.com/api/v1/match?object_one=' + x + '&object_two=' + y)  # api call to return the result of the battle
+    result = fightResult.json()  # json parsing of result
     winner = result['winner']
     loser = result['loser']
     outcome = result['outcome']
-    print(winner + " " + outcome + " " + loser)
+    print(winner + " " + outcome + " " + loser)  # output parsed result text
 
-
-fightResult(player1, player2)
+# main
+try:
+    while True:
+        os.system("cls")
+        fightResult(player1, player2)
+        input("Press enter to battle again or Ctrl+C to exit!")
+except KeyboardInterrupt:
+    pass
